83e92778aafe6e2833318cfacb8eb36897453a36
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-05-03 13:48:39 -0300

    Update mapa.py

--- EGG/mapa.py
+++ EGG/mapa.py
@@ -1,16 +1,16 @@
 import pandas as pd
 from pyx.dataframe_utility import join
-from EGG.hatchery import production
+from EGG.hatchery import breed, production
 
 def summary(objs, by): return [x.groupby(by=by).sum(numeric_only=True).reset_index() for x in objs]
 
 def MAPA_production(clas, inc, nasc):
 
   for df in [clas, inc, nasc]:
-	  df['MTECH_FLOCK_ID'] = df['FARM_CODE'].map(lambda x: '/'.join(df[df['FARM_CODE'] == x]['MTECH_FLOCK_ID'].unique()))
-	  #df['BREED'] = df['STRAIN_CODE'].map(lambda x: breed(x))
+	  df['MTECH_FLOCK_ID'] = df['FARM_CODE'].map(lambda x: '/'.join(sorted(df[df['FARM_CODE'] == x]['MTECH_FLOCK_ID'].unique())))
+	  df['BREED'] = df['STRAIN_CODE'].map(lambda x: breed(x))
 
-  on = ['FARM_CODE', 'MTECH_FLOCK_ID']#, 'BREED']
+  on = ['FARM_CODE', 'MTECH_FLOCK_ID', 'BREED']
   data = join(summary([clas, inc, nasc], on), on)
 
   data['TOTAL SALEABLE_2'] = data.apply(lambda x: x['TO_CHICKS_2'] - (x['PRE_SEX_CULLS_2'] + x['PRIME_CULLS_2']), axis=1)
@@ -20,14 +20,14 @@ def MAPA_production(clas, inc, nasc):
 
   data['PRODUCTION'] = data['FARM_CODE'].map(lambda x: production(x))
 
-  """new_columns = [
+  new_columns = [
 	  'Identificação dos ovos', 'Origem dos ovos', 'Granja de origem', 'Núcleo de produção', 'Linhagem', 'Ovos recebidos', 'Ovos incubados',
 	  'Pintos nascidos', 'Pintos vendáveis'
   ]
 
   data = data.rename(columns=dict(zip(['MTECH_FLOCK_ID', 'PRODUCTION', 'FARM_NAME', 'FARM_CODE', 'BREED', 'EGGS', 'EGGS_1', 'TO_CHICKS_2', 'TOTAL SALEABLE_2'], new_columns)))
 
-  data = data.reindex(columns=new_columns)"""
+  data = data.reindex(columns=new_columns)
 
   print(data)
   return data
