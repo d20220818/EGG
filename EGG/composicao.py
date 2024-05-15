2a1017cd779895ec57db1c95698f8daaa9eab2cd
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-05-15 12:01:10 -0300

    Update composicao.py

--- EGG/composicao.py
+++ EGG/composicao.py
@@ -47,7 +47,7 @@ def fillgtas(sheet, gtas):
 		sheet['GTA_NUMBER'] = ''
 	gta = sheet.columns.get_loc('GTA_NUMBER')
 	for i in range(sheet.shape[0]):
-		#print(sheet.iloc[i, gta])
+		print(sheet.iloc[i, gta])
 		if not isnull(sheet.iloc[i, gta]): continue
 		print(gtas)
 		idx = row_of(gtas, { 'PRODUCTION_DATE': sheet['PRODUCTION_DATE'][i], 'MTECH_FLOCK_ID': sheet['MTECH_FLOCK_ID'][i], 'STRAIN_CODE': sheet['STRAIN_CODE'][i] })
