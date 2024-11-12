f9431246974f0990b8becdc05ab8417fdcdbf4f1
d20220818 <159277773+d20220818@users.noreply.github.com>
2024-11-12 18:36:09 -0300

    Update composicao.py

--- EGG/composicao.py
+++ EGG/composicao.py
@@ -3,7 +3,7 @@ import math
 import numpy as np
 from pyx.xl import *
 from pyx.matrix_utility import transpose
-from pyx.pandasx import pick, strall, fillnext, resum, find_rows, split, select, segment, isnull#, row_of
+from pyx.pandasx import pick, strall, fillnext, resum, find_rows, split, segment, isnull#, row_of, select
 import pyx.pandasx as pdx
 from pyx.array_utility import concat
 from datetime import datetime as dt
